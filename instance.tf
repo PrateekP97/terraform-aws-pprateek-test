f10922a8f8fd50257508d524998866c519586cc2
PrateekP97 <prateekpal99810@gmail.com>
2026-03-24 08:57:13 +0000

    terraform-project

---
+++ instance.tf
@@ -0,0 +1,15 @@
+# Creating EC2 instance through terraform
+
+resource "aws_instance" "web" {
+  ami           = var.image_id
+  instance_type = var.instancetype
+  key_name      = aws_key_pair.key-tf.key_name
+
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "first-tf-instance"
+  }
+
+  user_data = file("${path.module}/script.sh")
+}
---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "ports" {
+  type = list(number)
+}
+
+variable "instancetype" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
